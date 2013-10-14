ec3e69b3e0c95b0c1c1cd5ff24b25a8136194e99
Jonathon Walker <jonathon.walker@student.uts.edu.au>
2013-10-15 00:34:24 +1100

    added bibliography details object but only has dummy data

--- popup.js
+++ popup.js
@@ -1,23 +1,58 @@
-var Tab = function(title) {
+var Tab = function(chromeTab) {
     var self = this;
-    self.title = title;
+    
+    self.getAuthor = function() {
+        return "Smith, J."
+    };
+    
+    self.getCreated = function() {
+        return (new Date()).toDateString();
+    };
+    
+    self.getWebsiteType = function() {
+        return '';
+    };
+    
+    self.getOrganisation = function() {
+        return self.getAuthor();
+    };
+    
+    self.getLocation = function() {
+        return 'Sydney';
+    };
+    
+    self.details = {
+        author: self.getAuthor(),
+        created: self.getCreated(),
+        title: chromeTab.title,
+        type: self.getWebsiteType(),
+        organisation: self.getOrganisation(),
+        location: self.getLocation(),
+        accessed: (new Date()).toDateString(),
+        url: '<' + chromeTab.url + '>'
+    };
+    
     self.convert = ko.observable(true);
-    self.toggleChecked = function() {
-        self.convert(!self.convert());
-    }
+    
+    self.toBibliography = function() {        
+        return _.map(self.details, function(detail) {
+            return detail;
+        }).join(', ');
+    };
 }
 
 var Page = function(tabs) {
     var self = this;
     self.tabs = ko.observableArray(tabs);
+    // self.bibliographies gets all ticked tabs and returns their bibliography.
 }
 
 $(document).ready(function() {
-    chrome.tabs.query({}, function(tabs) {
-        var allTabs = _.map(tabs, function(tab) {
-            return new Tab(tab.title);
+    chrome.tabs.query({}, function(chromeTabs) {
+        var tabs = _.map(chromeTabs, function(tab) {
+            return new Tab(tab);
         });
         
-        ko.applyBindings(new Page(allTabs));
+        ko.applyBindings(new Page(tabs));
     });
 });
